9263df3a716124363d1b433f80d902d2c028cb14
Antoine Basset <antoine.basser@cnes.fr>
2023-04-25 16:32:41 +0000

    Add complex interpolant test

--- Challenger/tests/src/Interpolant_test.cpp
+++ Challenger/tests/src/Interpolant_test.cpp
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_SUITE(Interpolant_test)
 
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE(sum_test) {
+BOOST_AUTO_TEST_CASE(real_test) {
   const std::vector<double> x {1, 2, 3, 4};
   const std::vector<double> y {1, 10, 100, 1000};
   const Linx::Raster<double> z(
@@ -21,6 +21,20 @@ BOOST_AUTO_TEST_CASE(sum_test) {
   const auto out = interpolant(2.5, 25);
   BOOST_TEST(out > 20);
   BOOST_TEST(out < 300);
+}
+
+BOOST_AUTO_TEST_CASE(complex_test) {
+  const std::vector<double> x {1, 2, 3, 4};
+  const std::vector<double> y {1, 10, 100, 1000};
+  const Linx::Raster<double, 3> z(
+      {x.size(), y.size(), 2},
+      {1, 2, 3, 4, 10, 20, 30, 40, 100, 200, 300, 400, 1000, 2000, 3000, 4000,
+       0, 0, 0, 0, 0,  0,  0,  0,  0,   0,   0,   0,   0,    0,    0,    0});
+  Challenger::ComplexInterpolant2D interpolant(x, y, z);
+  const auto out = interpolant(2.5, 25);
+  BOOST_TEST(out.real() > 20);
+  BOOST_TEST(out.real() < 300);
+  BOOST_TEST(out.imag() == 0);
 }
 
 //-----------------------------------------------------------------------------
