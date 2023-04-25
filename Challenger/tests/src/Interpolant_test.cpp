53a01365f2b867ae14e5984ae29461f64bf334fd
Antoine Basset <antoine.basser@cnes.fr>
2023-04-25 16:18:34 +0000

    Add Interpolant2D test

---
+++ Challenger/tests/src/Interpolant_test.cpp
@@ -0,0 +1,28 @@
+/// @copyright 2023, Antoine Basset
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "Challenger/Interpolant.h"
+
+#include <boost/test/unit_test.hpp>
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE(Interpolant_test)
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(sum_test) {
+  const std::vector<double> x {1, 2, 3, 4};
+  const std::vector<double> y {1, 10, 100, 1000};
+  const Linx::Raster<double> z(
+      {x.size(), y.size()},
+      {1, 2, 3, 4, 10, 20, 30, 40, 100, 200, 300, 400, 1000, 2000, 3000, 4000});
+  Challenger::Interpolant2D interpolant(x, y, z);
+  const auto out = interpolant(2.5, 25);
+  BOOST_TEST(out > 20);
+  BOOST_TEST(out < 300);
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE_END()
