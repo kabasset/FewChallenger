979f7a1e69530abcce21758bffd3e621f7ff9333
Antoine Basset <antoine.basser@cnes.fr>
2023-04-25 16:19:29 +0000

    Use identical test values for p, e

--- Challenger/tests/src/Amplitude_test.cpp
+++ Challenger/tests/src/Amplitude_test.cpp
@@ -12,15 +12,20 @@ BOOST_AUTO_TEST_SUITE(Amplitude_test)
 //-----------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_CASE(sum_test) {
+  std::cout << "Creating carrier..." << std::endl;
   static constexpr int lmax = 10;
   static constexpr int nmax = 30;
   Challenger::AmplitudeCarrier carrier(lmax, nmax);
+  std::cout << "  done." << std::endl;
 
   // FIXME many values
+  std::cout << "Interpolating..." << std::endl;
   std::vector<Challenger::Pe> pes {Challenger::Pe {4.5, 14.5}};
   std::vector<Challenger::Lmn> lmns {Challenger::Lmn {5, 5, 5}};
 
   const auto out = carrier.interpolate(pes, lmns);
+
+  std::cout << "  " << out << std::endl;
 }
 
 //-----------------------------------------------------------------------------
--- Few/tests/src/Amplitude_test.cpp
+++ Few/tests/src/Amplitude_test.cpp
@@ -12,13 +12,16 @@ BOOST_AUTO_TEST_SUITE(Amplitude_test)
 //-----------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_CASE(sum_test) {
+  std::cout << "Creating carrier..." << std::endl;
   static constexpr int lmax = 10;
   static constexpr int nmax = 30;
   Few::AmplitudeCarrier carrier(lmax, nmax, "");
+  std::cout << "  done." << std::endl;
 
   // FIXME many values
-  std::vector<double> ps {1.};
-  std::vector<double> es {1.};
+  std::cout << "Interpolating..." << std::endl;
+  std::vector<double> ps {4.5};
+  std::vector<double> es {14.5};
   std::vector<int> ls {5};
   std::vector<int> ms {5};
   std::vector<int> ns {5};
@@ -27,6 +30,12 @@ BOOST_AUTO_TEST_CASE(sum_test) {
   std::vector<std::complex<double>> out(n * nmodes);
 
   carrier.Interp2DAmplitude(out.data(), ps.data(), es.data(), ls.data(), ms.data(), ns.data(), n, nmodes);
+
+  std::cout << "  ";
+  for (const auto& e : out) {
+    std::cout << e << " ";
+  }
+  std::cout << std::endl;
 }
 
 //-----------------------------------------------------------------------------
