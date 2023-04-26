32e6048227f4178787234cd06b9401f2c06b77d1
Antoine Basset <antoine.basser@cnes.fr>
2023-04-26 09:06:30 +0000

    Add full l, m, n space test

--- Challenger/tests/src/Amplitude_test.cpp
+++ Challenger/tests/src/Amplitude_test.cpp
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_SUITE(Amplitude_test)
 
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE(sum_test) {
+BOOST_AUTO_TEST_CASE(singleton_test) {
   std::cout << "Creating carrier..." << std::endl;
   static constexpr int lmax = 10;
   static constexpr int nmax = 30;
@@ -23,6 +23,29 @@ BOOST_AUTO_TEST_CASE(sum_test) {
   std::vector<Challenger::Pe> pes {Challenger::Pe {4.5, 14.5}};
   std::vector<Challenger::Lmn> lmns {Challenger::Lmn {5, 5, 5}};
 
+  const auto out = carrier.interpolate(pes, lmns);
+
+  std::cout << "  " << out << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(full_lmn_test) {
+  std::cout << "Creating carrier..." << std::endl;
+  static constexpr int lmax = 10;
+  static constexpr int nmax = 30;
+  Challenger::AmplitudeCarrier carrier(lmax, nmax);
+  std::cout << "  done." << std::endl;
+
+  // FIXME many values
+  std::cout << "Interpolating..." << std::endl;
+  std::vector<Challenger::Pe> pes {Challenger::Pe {4.5, 14.5}};
+  std::vector<Challenger::Lmn> lmns;
+  for (Linx::Index l = 2; l <= lmax; ++l) { // Idem
+    for (Linx::Index m = 0; m <= l; ++m) {
+      for (Linx::Index n = -nmax; n <= nmax; ++n) {
+        lmns.push_back({l, m, n});
+      }
+    }
+  }
   const auto out = carrier.interpolate(pes, lmns);
 
   std::cout << "  " << out << std::endl;
